847fb8986e33b7b0182135d4d11f046307f10876
Gloria Estrella <gloriaaguilerasud@gmail.com>
2023-02-20 23:39:04 -0600

    Finish prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Si solo pones console.write vas a poder poner el input al lado de la pregunta
+        Console.Write ("What's your first name? ");
+        string first = Console.ReadLine();
+
+
+        Console.Write ("What's your last name? ");
+        string last = Console.ReadLine();
+
+        //el$ ayuda a que ahi mismo puedas poner los estrings con variables
+        Console.WriteLine($"Your name is {last}, {first} {last}");
+
     }
 }
